6df31b7c6a1a0d105ee96e8e79607ba8e7fbec5d
Katie Hansen <khansen0415@gmail.com>
2019-12-20 10:38:43 -0500

    change api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://cryptic-crag-09296.herokuapp.com/api',
     TOKEN_KEY: 'client-auth-token',
     API_KEY: process.env.REACT_APP_MAP_KEY,
 }
